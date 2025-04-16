3ed49d2a4dcfffd22c2519dea36cd3383e269b24
Shuvo Kumer Das <shuvocasanova@gmail.com>
2025-04-16 18:42:38 +0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -27,10 +27,16 @@ setupFolders();
 // Create Express app
 const app = express();
 
+
 // Set up middleware
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.use(helmet());
 app.use(compression());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
